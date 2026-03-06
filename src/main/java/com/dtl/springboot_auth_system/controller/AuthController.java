662aa1e4bc8f37c1a399f4cb6342b17b127e9cdc
hhuyyyyyy13012004 <hoanghuyn98@gmail.com>
2026-03-06 23:35:35 +0700

    otherPage

--- src/main/java/com/dtl/springboot_auth_system/controller/AuthController.java
+++ src/main/java/com/dtl/springboot_auth_system/controller/AuthController.java
@@ -8,12 +8,38 @@ public class AuthController {
 
     @GetMapping("/register")
     public String showRegisterPage() {
-        return "register";
+        return "auth/register";
     }
+
     @GetMapping("/login")
     public String showLoginPage() {
-        return "login";
+        return "auth/login";
     }
-   
 
+    @GetMapping("/profile")
+    public String showProfilePage() {
+        // Trỏ đúng vào thư mục user/ đã tạo
+        return "user/profile";
+    }
+
+    @GetMapping("/admin/dashboard")
+    public String showAdminDashboard() {
+        // Trỏ vào thư mục admin/ file admin-dashboard.html
+        return "admin/admin-dashboard";
+    }
+
+    @GetMapping("/403")
+    public String accessDenied() {
+        return "error/403";
+    }
+
+    @GetMapping("/forgot-password")
+    public String showForgotPasswordPage() {
+        return "auth/forgot-password";
+    }
+
+    @GetMapping("/reset-password")
+    public String showResetPasswordPage() {
+        return "auth/reset-password";
+    }
 }
---
+++ src/main/resources/static/js/auth.js
@@ -0,0 +1,6 @@
+// Đây là file xử lý logic demo cho khách hàng xem
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Auth JS đã sẵn sàng!");
+
+  // Sau này chúng ta sẽ viết logic Fetch API ở đây để nâng cấp lên React
+});
