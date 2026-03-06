6fd025d2c339676469aa124f7c68bc04d510f8e8
DoThanhLoc2334 <locdothanh233@gmail.com>
2026-03-06 09:05:40 +0700

    style(login-pages): implements style css for login pages

--- src/main/java/com/dtl/springboot_auth_system/controller/AuthController.java
+++ src/main/java/com/dtl/springboot_auth_system/controller/AuthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthController {
 
-    @GetMapping("/")
+    @GetMapping("/register")
     public String showRegisterPage() {
         return "register";
     }
