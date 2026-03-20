660764ca5555d8f0e61350783be9eee889f841d7
DoThanhLoc2334 <locdothanh233@gmail.com>
2026-03-20 08:26:19 +0700

    feat(database): implements structure database for users and roles

---
+++ backend/src/main/java/com/dtl/springboot_auth_system/model/Role.java
@@ -0,0 +1,35 @@
+package com.dtl.springboot_auth_system.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
