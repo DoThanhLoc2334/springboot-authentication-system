07049435b1bbd09c005d5817504bfb089d61925f
DoThanhLoc2334 <locdothanh233@gmail.com>
2026-04-01 23:40:52 +0700

    feat: implement OTP generation, verification, and password reset logic

--- backend/src/main/java/com/dtl/springboot_auth_system/service/AuthService.java
+++ backend/src/main/java/com/dtl/springboot_auth_system/service/AuthService.java
@@ -3,6 +3,9 @@ package com.dtl.springboot_auth_system.service;
 import com.dtl.springboot_auth_system.dto.request.RegisterRequest;
 import com.dtl.springboot_auth_system.dto.request.LoginRequest;
 import com.dtl.springboot_auth_system.dto.request.RefreshTokenRequest;
+import com.dtl.springboot_auth_system.dto.request.ForgotPasswordRequest;
+import com.dtl.springboot_auth_system.dto.request.VerifyOtpRequest;
+import com.dtl.springboot_auth_system.dto.request.ResetPasswordRequest;
 import com.dtl.springboot_auth_system.dto.response.JwtResponse;
 public interface AuthService {
     void register(RegisterRequest request);
@@ -12,4 +15,10 @@ public interface AuthService {
     JwtResponse refreshToken(RefreshTokenRequest request);
 
     void logoutCurrentUser();
+
+    void forgotPassword(ForgotPasswordRequest request);
+
+    void verifyOtp(VerifyOtpRequest request);
+
+    void resetPassword(ResetPasswordRequest request);
 }
